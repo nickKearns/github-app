f4e6308a02704ae6ec0406e763410f7b77cd5e36
Mohammad Azam <azamsharp@gmail.com>
2021-05-14 15:31:06 -0500

    Initial Commit

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Mohammad Azam on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
